2caac328b15fb42a762dcfee5ff4af075e5380d4
Leya <leya.min@mail.ru>
2025-12-17 14:44:01 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import '../src/components/SearchBar';
+import SearchBar from '../src/components/SearchBar';
+function App() {
+  return (
+		<div>
+
+
+		</div>
+  );
+}
+
+export default App;
